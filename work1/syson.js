48992daaf90da5fb25837e5a7feb35a139fb048e
Gentil250 <gentilniyifasha250@gmail.com>
2024-09-18 19:30:01 +0300

    second commit

---
+++ work1/syson.js
@@ -0,0 +1,116 @@
+var a, b;
+a=5, b=7;
+if (a>b)
+{
+    console.log(a);
+}
+else if (a<b)
+{
+    console.log(b);
+}
+else
+{
+    console.log("exit number");
+}
+
+
+var day=7;
+switch (day) {
+
+
+case 1: 
+    console.log("monday");
+    break;
+
+    case 2 :
+    
+        console.log("tuesday");
+        break;
+
+        case 3:
+            console.log("wednesday");
+            break;
+            default:
+
+        
+            console.log("exit");
+            break;
+}
+            
+            //function call
+        
+            function addNumbers(number1,number2)
+            {
+                return number1 + number2;
+                }
+
+                //function call
+
+                let result = addNumbers(5, 3);
+                console.log(result);
+
+               // ternal operator
+
+                var a;
+                a=4;
+                const k=(a>2) ? console.log("nuber is odd"):
+                console.log("number is even");
+
+                var sale;
+                sale=true;
+                sale=false;
+                const f=(sale)? console.log("time to wait"):
+                console.log("time to wait sale");
+
+                const h=4;
+                   
+                
+                const t=(h===6)? console.log("number is 6"):
+                console.log("number is not 6");
+
+
+                var vote;
+                vote=19;
+                const l=(vote>=18)? console.log("eligible to vote"):
+                console.log("is not eligible to vote");
+                //
+
+                var age=30, degree="a0";
+                if (age>=30 && degree==="a0"){
+                    console.log("this person are eligible");}
+                else {
+                console.log("the person are not allowed");}
+                //
+                var degree="software";
+                if(degree==="it" || degree==="computer science"){
+                    console.log("welcome");
+                }
+                else {
+                    console.log("not allowed");
+                }
+
+                //
+
+                var student="gentil";
+                if (!(student==="hello")) {
+                    console.log("this guy are not serious");
+                
+                }
+else{
+    console.log("this is a student");
+}
+// 
+
+
+
+
+
+
+
+
+
+            
+
+        
+    
+
