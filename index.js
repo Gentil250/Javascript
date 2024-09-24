e52f0a5f0ddb0db650a963ffb844af42e7412f78
Gentil250 <gentilniyifasha250@gmail.com>
2024-09-24 11:30:19 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-var a="Hello wold";
-console.log(a);
+// Ternal operator
+
+var a;
+a=30;
+const  j=(a<20)? console.log("intelligent"):
+
+console.log("false mind");
+
+//
+
+var vote;
+vote=18;
+const s=(vote<12)? console.log("eigible to vote"):
+console.log("this guy are not");
+//
+
+var age=30; degree="a0";
+if (age>=30 && degree==="it"){
+console.log ("welcome");
+}
+else console.log ("this guy are not");
+
+//
+var student="gentil";
+if(!(student="gentil")){
+    console.log("false name");
+    
+}
+else
+console.log("true name");
+
+
--- work1/syson.js
+++ work1/syson.js
@@ -63,7 +63,7 @@ case 1:
                 console.log("time to wait sale");
 
                 const h=4;
-                   
+                   //comparison operator
                 
                 const t=(h===6)? console.log("number is 6"):
                 console.log("number is not 6");
@@ -99,7 +99,143 @@ case 1:
 else{
     console.log("this is a student");
 }
-// 
+
+
+// function
+
+
+    function name()
+    
+
+{
+    console.log("welcome");
+
+}
+name()
+
+
+
+
+function x(name="gentil")
+{
+    console.log(` hello ${name}`)
+}
+
+x("aime")
+//
+function d(name,product)
+{
+    console.log ("thank you " +[name] ,"purchasing "  +[product])
+}
+ d("gentil", "shoes");
+
+ //
+ function s(length,widith)
+ {
+console.log(length * widith )
+ }
+ s(50,10)
+ //
+ function N(name)
+ {
+    console.log(`conguratulation ${name}`)
+ }
+ N("Alice");
+ //
+
+ function g(item="milk",item1="bread")  //  default parameter
+ {
+    console.log("remember to buy " + [item], "and " +[item1])
+ }
+ 
+ 
+ //
+function rectarea(w,l)
+{
+ var area= w*l
+ 
+ return area
+ }
+ console.log(rectarea(3,4))
+
+ function info(item3="emai", item4="age")
+ 
+{
+    var enter="allowed"
+if (item3==="email" && item4==="age"){
+    return enter
+}
+
+
+    return "person are not allowed"
+
+}
+ 
+ console.log (info())
+
+ //
+
+ function ss(product="sugar" , product2="ice")
+ {
+    console.log("remember to buy " + [product] , "and " + [product2]);
+
+ }
+ 
+ ss()
+ //
+
+ function info(item3="emai", item4="age")
+ 
+ {
+     var enter="allowed"
+ if (item3==="email" && item4==="age"){
+     return enter
+ }
+ 
+ 
+     return "person are not allowed"
+ 
+ }
+  
+  console.log (info())  //
+
+function gg(n1=10, n2=8, n3=5)
+{
+
+ if(n1>n2 && n2>n3){
+        return n1
+}
+else if (n2>n1 && n1>n2){
+    return n2
+}
+else {
+   
+ } return n3
+}
+console.log(gg())
+//console.log(g(10))
+
+
+  
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
 
 
 
